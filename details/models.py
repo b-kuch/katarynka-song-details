bd23c861dfcba3e99a08ac8f7d9d3883aa827df0
b-kuch <58431879+b-kuch@users.noreply.github.com>
2023-01-12 19:36:53 +0100

    extend models

--- details/models.py
+++ details/models.py
@@ -6,13 +6,28 @@ from django.db import models
 class Artist(models.Model):
     artist_name = models.CharField(max_length=300)
 
+    def __str__(self):
+        return self.artist_name
+
 
 class Album(models.Model):
     album_name = models.CharField(max_length=300)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.artist} - {self.album_name}"
+
+    def identifier(self) -> str:
+        return f"{self.artist}_{self.album_name}"
 
 
 class Song(models.Model):
     song_name = models.CharField(max_length=300)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.artist} - {self.song_name}"
+
+    def identifier(self) -> str:
+        return f"{self.artist}_{self.album_name}_{self.song_name}"
