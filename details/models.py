3b7afaad70d4a8154e5370c24ac275abe56b77d6
b-kuch <58431879+b-kuch@users.noreply.github.com>
2023-01-12 20:25:15 +0100

    add song list view

--- details/models.py
+++ details/models.py
@@ -1,33 +1,62 @@
+import json
+
 from django.db import models
 
 # Create your models here.
 
 
 class Artist(models.Model):
-    artist_name = models.CharField(max_length=300)
+    artist_name : "str" = models.CharField(max_length=300)
 
     def __str__(self):
         return self.artist_name
 
+    def identifier(self) -> str:
+        return f"{self.artist_name}"
+
+    def toDict(self):
+        return {
+            "name": self.artist_name,
+            "identifier": self.identifier()
+        }
+
 
 class Album(models.Model):
-    album_name = models.CharField(max_length=300)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    album_name : "str" = models.CharField(max_length=300)
+    artist : "Artist" = models.ForeignKey(Artist, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.artist} - {self.album_name}"
 
     def identifier(self) -> str:
-        return f"{self.artist}_{self.album_name}"
+        return f"{self.artist.artist_name}-{self.album_name}"
+
+    def toDict(self):
+        return {
+            "album": self.album_name,
+            "identifier": self.identifier()
+        }
 
 
 class Song(models.Model):
-    song_name = models.CharField(max_length=300)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    song_name : "str" = models.CharField(max_length=300)
+    artist : "Artist" = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    album : "Album" = models.ForeignKey(Album, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.artist} - {self.song_name}"
 
     def identifier(self) -> str:
-        return f"{self.artist}_{self.album_name}_{self.song_name}"
+        return f"{self.artist.artist_name}-{self.album.album_name}-{self.song_name}"
+
+    def filename(self) -> str:
+        return f"{self.artist.artist_name} - {self.song_name}.mp3"
+
+    def toDict(self):
+        return {
+            "song_name": self.song_name,
+            "artist": self.artist.toDict(),
+            "album": self.album.toDict(),
+            "filename": self.filename(),
+            "identifier": self.identifier()
+        }
--- details/views.py
+++ details/views.py
@@ -1,8 +1,17 @@
+import json
+
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Song
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def trending_songs(request):
+    return HttpResponse(json.dumps(list(map(Song.toDict, Song.objects.all()))),
+                        content_type="application/json")
